600099a0ac710dd3faf055263af0ac67878569bd
Ahmed Elsayed <ahmeding.hosam@gmail.com>
2025-09-19 22:40:40 +0100

    feat: added a little router to make widget NextJs agnostic

--- apps/widget/components/widget-view.tsx
+++ apps/widget/components/widget-view.tsx
@@ -1,13 +1,33 @@
 "use client";
 
+import { useAtomValue } from "jotai";
+import { JSX } from "react";
+
+import { screenAtom } from "@/atoms/screen";
+import { WidgetScreen } from "@/types";
+
 import WidgetAuthScreen from "./auth-screen";
 import WidgetFooter from "./footer";
 import WidgetHeader from "./header";
 
+const screenComponents: Record<WidgetScreen, JSX.Element> = {
+  error: <p>TODO: Error</p>,
+  loading: <p>TODO: Loading</p>,
+  selection: <p>TODO: Selection</p>,
+  voice: <p>TODO: Voice</p>,
+  auth: <WidgetAuthScreen />,
+  inbox: <p>TODO: Inbox</p>,
+  chat: <p>TODO: Chat</p>,
+  contact: <p>TODO: Contact</p>,
+};
+
 const WidgetView = ({ organizationId }: { organizationId: string }) => {
+  const screen = useAtomValue(screenAtom);
+
   return (
     <main className="min-w-screen bg-muted flex h-full min-h-screen flex-col overflow-hidden rounded-xl border">
-      <WidgetAuthScreen />
+      {screenComponents[screen]}
+      {/* <WidgetAuthScreen /> */}
       {/* <WidgetFooter /> */}
     </main>
   );
---
+++ apps/widget/types.ts
@@ -0,0 +1,9 @@
+export type WidgetScreen =
+  | "error"
+  | "loading"
+  | "selection"
+  | "voice"
+  | "auth"
+  | "inbox"
+  | "chat"
+  | "contact";
